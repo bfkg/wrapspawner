68ce8c3f296066ea46888975aaa27b7bcdc7c067
Benjamin Fersch <fersch@kit.edu>
2020-05-15 16:03:27 +0200

    log

--- wrapspawner/wrapspawner.py
+++ wrapspawner/wrapspawner.py
@@ -82,7 +82,7 @@ class WrapSpawner(Spawner):
             # initial state will always be wrong since it will see *our* state
             self.child_spawner.clear_state()
             print("Before checking state")
-            pprint(vars(self.child.state))
+            pprint(self.child_state)
             if self.child_state:
                 print("I dont have a state loding one...")
                 self.child_spawner.load_state(self.child_state)
